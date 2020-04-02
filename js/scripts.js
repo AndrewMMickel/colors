ecd15b710d4da743ef98fe30ae04537049678e1d
Andrew Mickel <andrew.m.mickel@gmail.com>
2020-04-02 09:07:04 -0700

    fixes to jscript

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
     $("button#green").click(function() {
-      $("body").addClass("green-background");
-      $("body").removeClass();
+        $("body").removeClass();
+        $("body").addClass("green-background");
     });
   
     $("button#yellow").click(function() {
-      $("body").addClass("yellow-background");
-      $("body").removeClass();
+        $("body").removeClass();
+        $("body").addClass("yellow-background");
     });
   
     $("button#red").click(function() {
-      $("body").addClass("red-background");
-      $("body").removeClass();
+        $("body").removeClass();
+        $("body").addClass("red-background");
+        
     });
   });
